f1fbc3e604d1d732258c50ac0b2e5d3ef2ffe715
imhjnoh <imhjnoh@gmail.com>
2022-10-07 01:42:18 +0900

    CREATE: emotion theme & globalstyle setup

---
+++ src/styles/DefaultTheme.ts
@@ -0,0 +1,11 @@
+import { Theme } from "@emotion/react";
+
+export const DefaultTheme: Theme = {
+  colors: {
+    primary: "#E3F53C",
+    secondary: "#FC942D",
+    positive: "#2CF273",
+    negative: "#E635C5",
+    bg: "#FFF2DD",
+  },
+};
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,80 @@
+import { css } from "@emotion/react";
+
+export const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline-style: none;
+  }
+  ul,
+  li {
+    list-style: none;
+    cursor: default;
+  }
+  a,
+  a:visited {
+    text-decoration: none;
+    color: inherit;
+  }
+  img {
+    border: 0;
+    vertical-align: middle;
+  }
+  html {
+    font-size: 16px;
+    @media (max-width: 1280px) {
+      font-size: 8px;
+    }
+    @media (max-width: 1024px) {
+      font-size: 10px;
+    }
+  }
+
+  body {
+    font-size: 1rem;
+    padding: 0;
+    font-family: "Pretendard";
+  }
+  h1,
+  h2,
+  h3,
+  h4 {
+    cursor: default;
+  }
+  p,
+  span {
+    cursor: default;
+  }
+  button {
+    cursor: pointer;
+  }
+
+  // 기본
+  @font-face {
+    font-family: "Pretendard";
+    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+      format("woff");
+    font-weight: 400;
+    font-style: normal;
+    font-display: swap;
+  }
+  // 500
+  @font-face {
+    font-family: "Pretendard";
+    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff")
+      format("woff");
+    font-weight: 500;
+    font-style: normal;
+    font-display: swap;
+  }
+  // 700
+  @font-face {
+    font-family: "Pretendard";
+    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff")
+      format("woff");
+    font-weight: 700;
+    font-style: normal;
+    font-display: swap;
+  }
+`;
